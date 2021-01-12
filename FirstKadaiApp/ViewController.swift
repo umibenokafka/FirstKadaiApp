a2431e2ea0c13a881226153b1a67bcc03cb85ef9
mitsuhisa.suzuki <umibeno_kafka@me.com>
2021-01-12 16:27:06 +0900

    FirstKadaiAppのラベルを追加した。

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 鈴木光央 on 2021/01/12.
+//  Copyright © 2021 taro.kirameki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
